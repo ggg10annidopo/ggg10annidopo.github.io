cc5ef1060d696229d55b83101dce9c872d5b1d4e
ggg10annidopo <119345637+ggg10annidopo@users.noreply.github.com>
2022-11-29 01:23:53 +0100

    commit

--- state/preload.js
+++ state/preload.js
@@ -87,11 +87,11 @@ preload.prototype = {
 
         this.game.load.spritesheet('cannone', 'img/cannone-sprite.png', 504.33, 428);
 
-        this.game.load.audio('menuaudio','audio/menuaudio.MP3');
-        this.game.load.audio('gameaudio','audio/Jammin.MP3');
-        this.game.load.audio('audiocannone','audio/cannone.MP3');
-        this.game.load.audio('audiofuochi','audio/fuochi.MP3');
-        this.game.load.audio('audioggg','audio/flauto.MP3');
+        this.game.load.audio('menuaudio','audio/menuaudio.mp3');
+        this.game.load.audio('gameaudio','audio/Jammin.mp3');
+        this.game.load.audio('audiocannone','audio/cannone.mp3');
+        this.game.load.audio('audiofuochi','audio/fuochi.mp3');
+        this.game.load.audio('audioggg','audio/flauto.mp3');
 
 	},
   	create: function(){
